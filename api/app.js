fedd94204c553683a733c2ec154440cbd33f2b56
choiru <c.zain@latrobe.edu.au>
2024-02-27 07:16:55 +1100

    1

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to cloud-based web application." });
 });
 
 require("./routes/tasks.routes")(app);
